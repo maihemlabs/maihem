65fe8e731fcdb5c8a233713b31a8229bee8b71d3
Simon White <simon@maihem.ai>
2024-09-05 10:49:14 +0100

    Schema updates

--- src/maihem/clients.py
+++ src/maihem/clients.py
@@ -1,11 +1,11 @@
 from typing import Dict, Literal
 
-from agents import AgentTarget
-from test_objects import Test, TestRun, TestRunResults
+from maihem.schemas.agents import AgentTarget
+from maihem.schemas.tests import Test, TestRun, TestRunResults
 
 
 class Client:
-    
+
     def __init__(self) -> None:
         pass
 
@@ -16,21 +16,109 @@ class Client:
         company: str,
         industry: str,
         description: str,
-        workflow: Dict = {},
-        rag_params: Dict = {}
+        workflow: Dict = None,
+        rag_params: Dict = None,
     ) -> AgentTarget:
         raise NotImplementedError("Method not implemented")
-    
+
     def get_target_agent(self, identifier: str) -> AgentTarget:
         raise NotImplementedError("Method not implemented")
-    
+
     def create_test(
         self,
         test_identifier: str,
         initiating_agent: Literal["maihem", "target"],
         agent_maihem_behavior_prompt: str = None,
         conversation_turns_max: int = 10,
-        metrics_config: Dict = {},
+        metrics_config: Dict = None,
+    ) -> Test:
+        raise NotImplementedError("Method not implemented")
+
+    def run_test(
+        self,
+        identifier: str,
+        test_identifier: str,
+        agent_target: AgentTarget,
+        # dynamic_mode: Literal["static", "dynamic"],
+        concurrent_conversations: int,
+    ) -> TestRun:
+        raise NotImplementedError("Method not implemented")
+
+    def get_test_run_results(test_run_identifier: str) -> TestRunResults:
+        raise NotImplementedError("Method not implemented")
+
+
+class Maihem(Client):
+
+    def __init__(self) -> None:
+        self.type = "sync"
+
+    def create_target_agent(
+        self,
+        identifier: str,
+        role: str,
+        company: str,
+        industry: str,
+        description: str,
+        workflow: Dict = None,
+        rag_params: Dict = None,
+    ) -> AgentTarget:
+        raise NotImplementedError("Method not implemented")
+
+    def get_target_agent(self, identifier: str) -> AgentTarget:
+        raise NotImplementedError("Method not implemented")
+
+    def create_test(
+        self,
+        test_identifier: str,
+        initiating_agent: Literal["maihem", "target"],
+        agent_maihem_behavior_prompt: str = None,
+        conversation_turns_max: int = 10,
+        metrics_config: Dict = None,
+    ) -> Test:
+        raise NotImplementedError("Method not implemented")
+
+    def run_test(
+        self,
+        identifier: str,
+        test_identifier: str,
+        agent_target: AgentTarget,
+        # dynamic_mode: Literal["static", "dynamic"],
+        concurrent_conversations: int,
+    ) -> TestRun:
+        raise NotImplementedError("Method not implemented")
+
+    def get_test_run_results(test_run_identifier: str) -> TestRunResults:
+        raise NotImplementedError("Method not implemented")
+
+
+class MaihemAsync(Client):
+
+    def __init__(self) -> None:
+        self.type = "async"
+
+    def create_target_agent(
+        self,
+        identifier: str,
+        role: str,
+        company: str,
+        industry: str,
+        description: str,
+        workflow: Dict = None,
+        rag_params: Dict = None,
+    ) -> AgentTarget:
+        raise NotImplementedError("Method not implemented")
+
+    def get_target_agent(self, identifier: str) -> AgentTarget:
+        raise NotImplementedError("Method not implemented")
+
+    def create_test(
+        self,
+        test_identifier: str,
+        initiating_agent: Literal["maihem", "target"],
+        agent_maihem_behavior_prompt: str = None,
+        conversation_turns_max: int = 10,
+        metrics_config: Dict = None,
     ) -> Test:
         raise NotImplementedError("Method not implemented")
 
@@ -41,97 +129,6 @@ class Client:
         agent_target: AgentTarget,
         dynamic_mode: Literal["static", "dynamic"],
         concurrent_conversations: int,
-    ) -> TestRun:
-        raise NotImplementedError("Method not implemented")
-
-    def get_test_run_results(test_run_identifier: str) -> TestRunResults:
-        raise NotImplementedError("Method not implemented")
-
-
-
-
-class Maihem(Client):
-    
-    def __init__(self) -> None:
-        self.type = "sync"
-    
-    def create_target_agent(
-        self,
-        identifier: str,
-        role: str,
-        company: str,
-        industry: str,
-        description: str,
-        workflow: Dict = {},
-        rag_params: Dict = {}
-    ) -> AgentTarget:
-        raise NotImplementedError("Method not implemented")
-    
-    def get_target_agent(self, identifier: str) -> AgentTarget:
-        raise NotImplementedError("Method not implemented")
-    
-    def create_test(
-        self,
-        test_identifier: str,
-        initiating_agent: Literal["maihem", "target"],
-        agent_maihem_behavior_prompt: str = None,
-        conversation_turns_max: int = 10,
-        metrics_config: Dict = {}
-    ) -> Test:
-        raise NotImplementedError("Method not implemented")
-
-    def run_test(
-        self,
-        identifier: str,
-        test_identifier: str,
-        agent_target: AgentTarget,
-        dynamic_mode: Literal["static", "dynamic"],              
-        concurrent_conversations: int      
-    ) -> TestRun:
-        raise NotImplementedError("Method not implemented")
-
-    def get_test_run_results(test_run_identifier: str) -> TestRunResults:
-        raise NotImplementedError("Method not implemented")
-        
-    
-    
-class MaihemAsync(Client):
-    
-    def __init__(self) -> None:
-        self.type = "async"
-        
-    def create_target_agent(
-        self,
-        identifier: str,
-        role: str,
-        company: str,
-        industry: str,
-        description: str,
-        workflow: Dict = {},
-        rag_params: Dict = {}
-    ) -> AgentTarget:
-        raise NotImplementedError("Method not implemented")
-    
-    def get_target_agent(self, identifier: str) -> AgentTarget:
-        raise NotImplementedError("Method not implemented")
-    
-    def create_test(
-        self,
-        test_identifier: str,
-        initiating_agent: Literal["maihem", "target"],
-        agent_maihem_behavior_prompt: str = None,
-        conversation_turns_max: int = 10,
-        metrics_config: Dict = {}
-    ) -> Test:
-        raise NotImplementedError("Method not implemented")
-
-    def run_test(
-        self,
-        identifier: str,
-        test_identifier: str,
-        agent_target: AgentTarget,
-        dynamic_mode: Literal["static", "dynamic"],              
-        concurrent_conversations: int      
     ) -> TestRun:
         raise NotImplementedError("Method not implemented")
 
