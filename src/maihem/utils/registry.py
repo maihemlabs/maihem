44d85926f3f1a8c23029fa578d4fecdb8b283cbf
e-candela <eduardo@maihem.ai>
2025-02-05 13:36:52 +0000

    move to utils

--- src/maihem/utils/registry.py
+++ src/maihem/utils/registry.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 
 function_registry = {}
 
@@ -8,5 +8,9 @@ def register_function(function: Callable, function_name: Optional[str] = None) -
     function_registry[function_name_dict] = function
 
 
-def call_function(function_name: str, *args, **kwargs) -> Callable:
+def call_function(function_name: str, *args, **kwargs) -> Any:
     return function_registry[function_name](*args, **kwargs)
+
+
+def get_function(function_name: str) -> Callable:
+    return function_registry[function_name]
